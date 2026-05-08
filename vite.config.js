a807297dc553ec78abe719e75449f7fda6bbfc97
siraphob sirichiraphan <75577213+SiraphobS@users.noreply.github.com>
2026-05-09 04:27:07 +0700

    fix: correct base path for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   // ตั้งค่า Base สำคัญมากสำหรับรันบน GitHub Pages (ชื่อ repository ของคุณ)
-  base: '/gunshadelivery/',
+  base: '/blacknight-nexus/',
   publicDir: 'image', // กำหนดให้โฟลเดอร์ image เป็นโฟลเดอร์ public
   build: {
     rollupOptions: {

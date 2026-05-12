6987510b84b0989529c17650e1204676d25be2e9
siraphob sirichiraphan <75577213+SiraphobS@users.noreply.github.com>
2026-05-12 12:25:51 +0700

    fix: add plugins array to vite config for wrangler compatibility

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [],
   // ตั้งค่า Base สำคัญมากสำหรับรันบน GitHub Pages (ชื่อ repository ของคุณ)
   base: '/blacknight-nexus/',
   publicDir: 'image', // กำหนดให้โฟลเดอร์ image เป็นโฟลเดอร์ public
